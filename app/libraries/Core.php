f28e18bf2b2e7c45c03bb48b8a490ceebef73b88
Eric Julianto <algonacci1@gmail.com>
2023-02-01 09:43:12 +0700

    still error

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -8,4 +8,31 @@
 class Core
 {
  protected $_currentController = "Pages";
+ protected $_currentMethod     = "index";
+ protected $_params            = [];
+
+ public function __construct()
+ {
+  $url = $this->getUrl();
+
+  if (file_exists("../app/controllers/" . ucwords($url[0]) . ".php")) {
+   $this->currentController = ucwords($url[0]);
+   unset($url[0]);
+  }
+
+  require_once "../app/controllers/" . $this->_currentController . ".php";
+
+  $this->currentController = new $this->_currentController;
+
+ }
+
+ public function getUrl()
+ {
+  if (isset($_GET["url"])) {
+   $url = rtrim($_GET["url"], "/");
+   $url = filter_var($url, FILTER_SANITIZE_URL);
+   $url = explode("/", $url);
+   return $url;
+  }
+ }
 }
